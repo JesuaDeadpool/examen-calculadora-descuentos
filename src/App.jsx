c430deed4bcf2da48eec696f91d07175b73dad10
Jesua <144072667+JesuaDeadpool@users.noreply.github.com>
2025-08-19 19:50:46 -0600

    Empezando la aplicacion de calculadora de descuentos

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CalculadoraDescuento from "./components/Calculadora-descuento"
+
+
+function App() {
+
+  return (
+    <CalculadoraDescuento />
+  )
+}
+
+export default App
---
+++ src/components/Calculadora-descuento.jsx
@@ -0,0 +1,24 @@
+
+const CalculadoraDescuento = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <label htmlFor=""></label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-6">
+                    <label htmlFor=""></label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col-6">
+                    <label htmlFor=""></label>
+                    <input type="text" className="form-control" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default CalculadoraDescuento; 
